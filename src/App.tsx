6c33f98b2907272ce5fff6eb3fb562fd90dac38a
Peem Phurit <phurit.dua.workspace@gmail.com>
2024-12-19 20:32:34 +0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {Navigate, Route, Routes} from 'react-router-dom';
 import Login from '@/pages/Login';
 import Dashboard from './pages/Dashboard';
 import {useAuth} from "./auth/AuthProvider.tsx";
+import Menu from "@/pages/Menu.tsx";
 
 const ProtectedRoute = ({children}: { children: JSX.Element }) => {
     const {token} = useAuth();
@@ -12,13 +13,19 @@ const ProtectedRoute = ({children}: { children: JSX.Element }) => {
 const App = () => {
     return (
         <Routes>
-            <Route path="/login" element={<Login/>}/>
+            <Route path="/" element={<Login/>}/>
             <Route
                 path="/dashboard"
                 element={
                     <ProtectedRoute>
                         <Dashboard/>
                     </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/menu"
+                element={
+                        <Menu/>
                 }
             />
         </Routes>
--- src/provider.tsx
+++
@@ -1,9 +0,0 @@
-import { NextUIProvider } from "@nextui-org/react";
-import { useNavigate } from "react-router-dom";
-import React from "react";
-
-export function Provider({ children }: { children: React.ReactNode }) {
-    const navigate = useNavigate();
-
-    return <NextUIProvider navigate={navigate}>{children}</NextUIProvider>;
-}
